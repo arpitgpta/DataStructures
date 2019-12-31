44f59d8be43f1409a3baec5c950cafbac590922f
arpitgpta <arpit.cs18@nitp.ac.in>
2019-12-31 16:09:53 +0530

    corrections done

--- nCrModM.cpp
+++ nCrModM.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 #define ull unsigned long long
 #define deb(x) cout << #x << " " << x << endl;
-#define MOD %17788007
-#define M 17788007
+#define MOD %1000000007  
+#define M 1000000007
 
 vector<ull> fact_mod_m(1e6, 0); // for memorising factorial of all numbers
 vector<ull> fact_inverse(1e6, 0); // to memorise ((x!)^(m-2))MOD 
@@ -30,13 +30,14 @@ ull power(ull p, ull q){
 
 ull ncr(ull n, ull r){
 
-    ull n_fact = fact(n)MOD;
-    ull nr_fact = fact(n-r)MOD;
-    ull r_fact = fact(r)MOD;
-    ull p1 = power(nr_fact, M-2)MOD;
-    ull p2 = power(r_fact, M-2)MOD;
+    // ull n_fact = fact(n)MOD;
+    // ull nr_fact = fact(n-r)MOD;
+    // ull r_fact = fact(r)MOD;
+    // ull p1 = power(nr_fact, M-2)MOD;
+    // ull p2 = power(r_fact, M-2)MOD;
 
-    return (((n_fact*p1)MOD)*p2)MOD;
+    return (((fact_mod_m[n]*fact_inverse[r])MOD)*fact_inverse[n-r])MOD;
+
 }
 
 void pre_processing(){
@@ -49,12 +50,21 @@ int main(){
     
     pre_processing();
     int t;
-    cin >> t;
-    while(t--){
-        int n, r;
-        cin >> n >> r;
-        cout << ncr(n, r) << endl;
+    // cin >> t;
+    // while(t--){
+    //     int n, r;
+    //     cin >> n >> r;
+    //     cout << ncr(n, r) << endl;
+    // }
+    int flag = 0;
+    for(int i = 0; i < 100000; i++){
+        ull t1 = ncr(100000, i);
+        ull t2 = ncr(100000, 100000-i);
+        if(t1 != t2){
+            cout << "Error in ncr at i = " << i << endl;
+            flag = 1;
+        }
     }
-
+    if(flag == 0) cout << "no error occured" << endl;
 
 }
