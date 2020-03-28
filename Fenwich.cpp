617246cf3797c646dc3b1861f274b44343042dc6
Arpit Gupta <55844150+arpitgpta@users.noreply.github.com>
2020-03-28 17:55:53 +0530

    Oops implemented

--- Fenwich.cpp
+++ Fenwich.cpp
@@ -26,13 +26,6 @@ class fenwich{
         }
 
         int next(int a){
-            // int b;
-            // b = a;
-            // b = !b;
-            // b++;
-            // b = b&a;
-            // b = a+b;
-            // return b;
             int b = (~a+1)&a;
             return a+b;
         }
@@ -54,29 +47,18 @@ class fenwich{
         fenwich(vin arr){
             this->arr = arr;
             int n = arr.size();
-            // deb(n);
             tree = vin(n + 1, 0);
             int temp;
-            // cout << "lets start" << endl;
             loop(n){
                 tree[i+1] += arr[i];
                 temp = next(i+1);
                 while(temp <= n){
-                    // deb2(temp, tree[temp]);
                     tree[temp] += arr[i];
-                    // deb(tree[temp]);
                     temp = next(temp);
-
                 }
-                // cout << "i = " << i << " done\n";
             }
-            // cout << "done" << endl;
-            // cout << "tree" << endl;
-            // Loop(1, n+1, i) cout << tree[i] << " "; cout << endl;
         }
         
-        fenwich(){}
-
         void update(int val, int ind){
             add_at(val-arr[ind], ind);
         }
@@ -97,88 +79,6 @@ class fenwich{
 };
 
 
-vin tree;
-
-int parent(int a){
-    int b = (~a+1)&a;
-    return a-b;    
-}
-
-int next(int a){
-    // int b;
-    // b = a;
-    // b = !b;
-    // deb(b);
-    // b++;
-    // deb(b);
-    // b = b&a;
-    // deb(b);
-    // b = a+b;
-    // deb(b);
-    // return b;
-    int b = (~a+1)&a;
-    return a+b;
-}
-
-void create_fenwich(vin arr){
-    int n = arr.size();
-    tree = vin(n + 1, 0);
-    int temp;
-    loop(n){
-        tree[i+1] = arr[i];
-        temp = next(i+1);
-        while(temp <= n){
-            tree[temp] += arr[i];
-            temp = next(temp);
-        }
-    }
-}
-
-void add_at(int val, int ind){
-    tree[++ind] += val;
-    ind = next(ind);
-    int m = tree.size();
-    while(ind < m){
-        tree[ind] += val;
-        ind = next(ind);
-    }
-}
-
-int sum(int a){
-    int ans = tree[++a];
-    a = parent(a);
-    while(a){
-        ans += tree[a];
-        a = parent(a);
-    }
-    return ans;
-}
-
-int sum(int a, int b){
-    return sum(b)-sum(a-1);
-}
-
-int32_t main1(){
-    cout << "next" << endl;
-    for(int i = 1; i < 10; i++){
-        int temp = i;
-        while(temp < 11){
-            cout << temp << " ";
-            temp = next(temp);
-        }
-        cout << endl;
-    }
-
-    cout << "parent" << endl;
-    for(int i = 0; i < 10; i++){
-        int temp = i;
-        while(temp){
-            cout << temp << " ";
-            temp = parent(temp);
-        }
-        cout << endl;
-    }
-}
 
 int32_t main(){
     int n;
@@ -187,16 +87,6 @@ int32_t main(){
     for(int i = 0; i < n; i++)    cin >> arr[i];
     fenwich f(arr);
 
-    cout << "parent" << endl;
-
-    for(int i = 0; i < 10; i++){
-        int temp = i;
-        while(temp){
-            cout << temp << " ";
-            temp = parent(temp);
-        }
-        cout << endl;
-    }
     for(auto i : f.tree) cout << i << " "; cout << endl << endl;
     Loop(1, n, i) cout << i << " " << f.sum(i) << endl;
 }
